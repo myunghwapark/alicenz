92eeb73359d7000ff993a2da403266744f9490a7
Myunghwa <minghoa@naver.com>
2020-03-02 12:01:38 +1300

    first commit

---
+++ db/database.php
@@ -0,0 +1,11 @@
+<?php
+
+	$host = "fdb23.biz.nf";
+	$database = "3332794_alicenz";
+	$user = "3332794_alicenz";
+	$password = "aldtkfkd2043";
+	
+	$connection = mysqli_connect($host, $user, $password, $database);
+
+	
+?>
